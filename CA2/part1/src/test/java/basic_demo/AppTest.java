c28ec6cc6466fa66a7d5c1e89dd6cd1541301de5
unknown <1240598@isep.ipp.pt>
2025-09-23 00:07:19 +0100

    CA2 Part1 completo

---
+++ CA2/part1/src/test/java/basic_demo/AppTest.java
@@ -0,0 +1,10 @@
+package basic_demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test void sanity() {
+        assertEquals(4, 2 + 2);
+    }
+}
