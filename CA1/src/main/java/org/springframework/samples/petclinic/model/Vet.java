ab6f66300eba04f365a23974c31d8da61e301b8c
unknown <1240598@isep.ipp.pt>
2025-09-30 16:00:41 +0100

    Merging conflits test

--- CA1/src/main/java/org/springframework/samples/petclinic/model/Vet.java
+++ CA1/src/main/java/org/springframework/samples/petclinic/model/Vet.java
@@ -64,7 +64,7 @@ public class Vet extends Person {
     private String email;
 
     public String getEmail() {
-        return 2;
+        return email2;
     }
 
     public void setEmail(String email) {
