a8e2c829b55eb7812a126295db4893bdc429600a
1240598Rafa <1240598@isep.ipp.pt>
2025-10-07 15:04:40 +0100

    Add Gradle tasks: runServer, test, backup, zipBackup

---
+++ CA2/Part1/backup/test/java/basic_demo/AppTest.java
@@ -0,0 +1,12 @@
+package basic_demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void testAppRuns() {
+        assertEquals(2, 1 + 1, "Gradle test environment working");
+    }
+}
