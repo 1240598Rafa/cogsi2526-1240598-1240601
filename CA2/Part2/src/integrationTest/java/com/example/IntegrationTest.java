62e7788cc880814ad1eb0bb07708054a1aa61911
fitnesscamacho-admin <xavier.ricarte@gmail.com>
2025-10-12 18:07:53 +0100

    IntegrationTest configured

---
+++ CA2/Part2/src/integrationTest/java/com/example/IntegrationTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IntegrationTest {
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+}
